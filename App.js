7c978e44be57bea59db5ac031956b1ea35ab6345
Sabine Holler <sabidasa@gmail.com>
2021-06-14 16:10:47 +0200

    home buttons

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './screens/Home'
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <StatusBar style="auto" />
+      <Stack.Navigator>  
+        <Stack.Screen name='To-Do' component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Home.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { Text, View, StyleSheet, TouchableOpacity, FlatList,} from 'react-native'
+import Colors from '../constants/Colors'
+import {Ionicons} from '@expo/vector-icons'
+
+
+const ListButton = ({title, color}) => {
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={()=>{}} style={[styles.itemContainer, {backgroundColor: color}]}> 
+                <View> 
+                    <Text style={styles.itemTitle}>{title}</Text>
+                </View>
+                <View style={{flexDirection:'row'}}> 
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Ionicons name='options-outline' size={24} color='white' />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{}}>
+                        <Ionicons name='trash-outline' size={24} color='white' />
+                    </TouchableOpacity>
+                </View>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+
+const Home = () => {
+    
+    return (
+        <View style={styles.container}>
+                <FlatList
+                    data={[{title:'School', color: Colors.red}, 
+                    {title:'Work', color: Colors.blue}, 
+                    {title:'Fun', color: Colors.green}]}
+                    renderItem={({item: {title, color}, index}) => {
+                        return( 
+                            <ListButton title={title} color={color}/>
+                        )
+                    }}
+
+                    />
+
+        </View>
+    )
+    
+}
+
+export default Home;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    itemTitle: { fontSize: 24, padding: 5, color: "white" },
+    itemContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 100,
+        flex: 1,
+        borderRadius: 20,
+        marginHorizontal: 20, 
+        marginVertical: 10,
+        padding: 15,
+    },
+    icon: {
+        padding: 5,
+        fontSize: 24,
+    },
+    centeredView: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 50,
+    },
+    modalView: {
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+});
+
+
